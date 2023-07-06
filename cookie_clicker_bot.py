97e3e300e522d974ef0c317137931601d4fa09cf
Omari_Mourning <Ojmourning06@gmail.com>
2023-07-05 21:21:08 -0500

    Web Driver
    Edited the web driver so that chrome can be accessed no matter what the path is.

--- cookie_clicker_bot.py
+++ cookie_clicker_bot.py
@@ -6,9 +6,10 @@ import time
 
 
 def main():
-    chromePath = 'C:\Windows\WinSxS\x86_netfx4-browser_files_b03f5f7f11d50a3a_4.0.15912.0_none_06d55e201f3d4256\chrome.browser'
-    broserDriver = Service(chromePath)
-    page = webdriver.Chrome(service=broserDriver)
+    
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    page = webdriver.Chrome(options=options, chrome_options=options)
 
     page.get('https://orteil.dashnet.org/cookieclicker/')
     time.sleep(2)
